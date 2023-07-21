d0fc2f2c51c5f3ae9dcef43e0966c30295a7bb54
robert-afriex <robert@afriex.co>
2023-07-21 19:36:24 -0300

    feat: colors

---
+++ frontend/public/colors.js
@@ -0,0 +1,9 @@
+module.exports = {
+  skyBlue: '#A4DCE2',
+  pastelPink: '#F7D9D7',
+  grassGreen: '#40CD7E',
+  softBeige: '#FCF5F0',
+  offWhite: '#FFFEFE',
+  dark: '#2E272A',
+  secondary: '#726B68'
+}
